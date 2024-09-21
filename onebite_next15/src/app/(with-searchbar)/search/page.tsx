36e33aee557063fc97b8d08698a00a9d8929d293
SeokHun <tommya98@naver.com>
2024-09-21 13:31:36 +0900

    App Router 시작하기

---
+++ onebite_next15/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,18 @@
+import BookItem from "@/components/book-item";
+import books from "@/mock/books.json";
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
